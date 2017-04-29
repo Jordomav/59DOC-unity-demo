86d14036e4682de15b7a9a637afdb070291d7dd0
Jordan Mavrogeorge <jordomav@gmail.com>
2017-04-29 12:25:44 -0700

    init

---
+++ reload.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class reload : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.GetComponent<Collider>().tag == "restart") {
+			SceneManager.LoadScene("Geek Night");
+		}
+	}
+}
