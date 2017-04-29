68f8babae2c4e428c191387609aea35634bbea8a
Jordan Mavrogeorge <jordomav@gmail.com>
2017-04-29 13:12:05 -0700

    menu controller

---
+++ menuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menuController : MonoBehaviour {
+
+	public Button startGame;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = startGame.GetComponent<Button> ();
+		btn.onClick.AddListener (BeginGame);
+	}
+
+	void BeginGame () {
+		SceneManager.LoadScene("Geek Night");
+	}
+}
